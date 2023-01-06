913b7bdcae5bb5b9532a65063fdb285a695ad1ce
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-06 14:51:47 -0700

    add event listener for submit button

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const submitBtn = document.getElementById('submit-button');
+const promptSection = document.getElementById('prompt');
+const fortuneSection = document.getElementById('fortune');
+const answerP = document.getElementById('answer');
+const resetBtn = document.getElementById('reset=button');
 /* State */
 
 /* Events */
-
+submitBtn.addEventListener('click', () => {
+    toggleSections();
+    const randomInt = Math.floor(Math.random() * answers.length);
+    const randomResponse = answers[randomInt];
+    answerP.textContent = randomResponse;
+});
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Outlook hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+/* Array for answers list */
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
